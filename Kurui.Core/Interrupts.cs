ecb52cfd6c206e06ad39feda211c996799e97dd3
Christopher Davis <thechristopheriandavis@gmail.com>
2018-03-20 14:25:36 -0400

    Added class for managing interrupts

---
+++ Kurui.Core/Interrupts.cs
@@ -0,0 +1,126 @@
+﻿namespace Kurui.Core
+{
+    internal class Interrupts
+    {
+        private InterruptRegister enable = 0, request = 0;
+
+        public byte EnableRegister
+        {
+            get => enable;
+            set => enable = value;
+        }
+
+        public byte RequestRegister
+        {
+            get => (byte) ( InterruptsEnabled ? request : 0 );
+            set
+            {
+                if (InterruptsEnabled)
+                    request = value;
+            }
+        }
+
+        public void EnableInterrupts()
+        {
+            enable.Enable();
+        }
+
+        public void DisableInterrupts()
+        {
+            enable.Disable();
+        }
+
+        public bool InterruptsEnabled => enable == 0;
+
+        public bool VBlankRequest
+        {
+            get => InterruptsEnabled && request.VBlank;
+            set
+            {
+                if (InterruptsEnabled) request.VBlank = value;
+            }
+        }
+
+        public bool LCDRequest
+        {
+            get => InterruptsEnabled && request.LCD;
+            set
+            {
+                if (InterruptsEnabled) request.LCD = value;
+            }
+        }
+
+        public bool TimerRequest
+        {
+            get => InterruptsEnabled && request.Timer;
+            set
+            {
+                if (InterruptsEnabled) request.Timer = value;
+            }
+        }
+
+        public bool SerialRequest
+        {
+            get => InterruptsEnabled && request.Serial;
+            set
+            {
+                if (InterruptsEnabled) request.Serial = value;
+            }
+        }
+
+        public bool JoypadRequest
+        {
+            get => InterruptsEnabled && request.Joypad;
+            set
+            {
+                if (InterruptsEnabled) request.Joypad = value;
+            }
+        }
+
+        private struct InterruptRegister
+        {
+            private byte reg;
+
+            private InterruptRegister(byte reg)
+            {
+                this.reg = reg;
+            }
+
+            public static implicit operator byte(InterruptRegister r) => r.reg;
+            public static implicit operator InterruptRegister(byte b) => new InterruptRegister(b);
+
+            public bool VBlank
+            {
+                get => ( reg & 1 ) == 1;
+                set => reg = value ? reg.SetBit(0) : reg.ClearBit(0);
+            }
+
+            public bool LCD
+            {
+                get => ( reg >> 1 & 1 ) == 1;
+                set => reg = value ? reg.SetBit(1) : reg.ClearBit(1);
+            }
+
+            public bool Timer
+            {
+                get => ( reg >> 2 & 1 ) == 1;
+                set => reg = value ? reg.SetBit(2) : reg.ClearBit(2);
+            }
+
+            public bool Serial
+            {
+                get => ( reg >> 3 & 1 ) == 1;
+                set => reg = value ? reg.SetBit(3) : reg.ClearBit(3);
+            }
+
+            public bool Joypad
+            {
+                get => ( reg >> 4 & 1 ) == 1;
+                set => reg = value ? reg.SetBit(4) : reg.ClearBit(4);
+            }
+
+            public void Enable()  => reg = 255;
+            public void Disable() => reg = 0;
+        }
+    }
+}
--- Scratchpad/Program.cs
+++ Scratchpad/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Running;
@@ -12,14 +13,39 @@ namespace Scratchpad
     {
         static void Main(string[] args)
         {
-            byte bankIndex = 1;
-            int value = 128;
-           
-            bankIndex = (byte)((bankIndex & 0b0011_1111) | (((byte)value) & 0b1100_0000));
-            Console.WriteLine(bankIndex);
+            Byl[] bls = new[]
+                {new Byl(BoolFlags.None), new Byl(BoolFlags.Zero), new Byl(BoolFlags.Three), new Byl(BoolFlags.Five),};
+            foreach (Byl byl in bls)
+            {
+                Console.WriteLine($"Bool: {byl.bul}");
+                Console.WriteLine($"Byte: {byl.byt}");
+            }
         }
-
     }
 
+    [StructLayout(LayoutKind.Explicit)]
+    struct Byl
+    {
+        [FieldOffset(0)] public byte byt;
+        [FieldOffset(0)] public bool bul;
 
+        public Byl(BoolFlags b)
+        {
+            bul = false;
+            byt = (byte) b;
+        }
+    }
+
+    enum BoolFlags
+    {
+        None = 0,
+        Zero  = 0b0000_0001,
+        One   = 0b0000_0010,
+        Two   = 0b0000_0100,
+        Three = 0b0000_1000,
+        Four  = 0b0001_0000,
+        Five  = 0b0010_0000,
+        Six   = 0b0100_0000,
+        Seven = 0b1000_0000
+    }
 }
